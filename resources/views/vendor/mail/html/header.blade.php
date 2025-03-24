f790b11fcb3d98728eb856b80d66ec7be46ad805
ABDUL ADI SYAZWI BIN MUHAMMAD JUNAIDI <23FTT1092@student.pb.edu.bn>
2025-03-24 17:15:50 +0800

    Added pb logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,10 @@
 @props(['url'])
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
-</tr>
+        <table class="header" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="align-center">
+                    <a href="{{ config('app.url') }}" style="display: inline-block;">
+                        <img src="{{ asset('images/pblogo.png') }}" alt="{{ config('app.name') }}">
+                    </a>
+                </td>
+            </tr>
+        </table>
